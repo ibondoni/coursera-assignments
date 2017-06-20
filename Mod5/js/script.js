7bcd420a520574b69b7e82f0e5d267ba561a506e
Ignacio Bondoni <ibondoni@gmail.com>
2017-06-20 10:25:02 -0300

    testing2

--- Mod5/js/script.js
+++ Mod5/js/script.js
@@ -97,7 +97,7 @@ function buildAndShowHomeHTML (categories) {
     homeHtmlUrl,
     function (homeHtml) {
       this.homepage = homeHtml.responseText;
-      console.log(homepage);
+      console.log(this.homepage);
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
